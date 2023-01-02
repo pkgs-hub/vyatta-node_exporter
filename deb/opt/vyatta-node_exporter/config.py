10b64856a049746e0003732a13053afdb11a5136
Nico Felbinger <26925347+felbinger@users.noreply.github.com>
2023-01-02 13:21:10 +0100

    fixed bugs

--- deb/opt/vyatta-node_exporter/config.py
+++ deb/opt/vyatta-node_exporter/config.py
@@ -43,6 +43,15 @@ def generate(node_exporter):
             os.unlink(config_file)
         return None
 
+    # merge web/listen-address with subelement web/listen-address/port
+    if 'web' in node_exporter:
+        # {'listen-address': {'0.0.0.0': {'port': '8080'}}
+        if 'listen-address' in node_exporter['web']:
+            address = list(node_exporter['web']['listen-address'].keys())[0]
+            port = node_exporter['web']['listen-address'][address].get("port", 9100)
+            node_exporter['web']['listen-address'] = f"{address}:{port}"
+            del node_exporter['web']['listen-address']['port']
+
     with open('/opt/vyatta-node_exporter/config.j2', 'r') as tmpl, open(config_file, 'w') as out:
         template = Template(tmpl.read()).render(data=node_exporter)
         out.write(template)
